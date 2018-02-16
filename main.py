aa13a0b5a3f1b996650925913cc3dcfcd30523a4
Kilian Koeltzsch <me@kilian.io>
2018-02-16 10:06:27 +0100

    add loglevel flag

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import os
+import logging
+import argparse
 
 from telegram.ext import Updater
 
 from oepnvdd_bot.monitor import *
 from oepnvdd_bot.search import *
 from oepnvdd_bot.route import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--log', dest='loglevel', default='ERROR', help='log level')
+args = parser.parse_args()
+
+numeric_level = getattr(logging, args.loglevel.upper(), None)
+if not isinstance(numeric_level, int):
+    raise ValueError('Invalid log level: {}'.format(args.loglevel))
+logging.basicConfig(level=numeric_level)
 
 
 if __name__ == '__main__':
